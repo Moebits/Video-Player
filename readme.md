a91803ae651e7dda88f10f5cc43f8af7a1e44e4a
Tenpi <pie4chan@gmail.com>
2021-08-21 00:38:25 -0400

    init

---
+++ readme.md
@@ -0,0 +1,30 @@
+## Music Player
+
+<img src="assets/images/readme.png">
+
+This is a music player that can apply cool effects such as reversing, time stretching, and pitch shifting in real-time.
+It's basically the GUI version of my [Music Player Web](https://github.com/Tenpi/Music-Player-Web) site.
+
+### Features:
+- Play, pause, loop and seek audio
+- Adjust the volume of the audio
+- Reverse audio in real-time
+- Time stretching in real-time (can either preserve or affect the pitch)
+- Pitch shifting in real-time (only if time-stretch is set to not affect the pitch)
+- Looping from point A to point B
+- Play local files, YouTube videos, and Soundcloud tracks
+- Render and download audio with effects
+- Keeps a record of recent plays
+- Play previous or next tracks (from your play history)
+
+### Installation
+
+Download the latest exe installer from the [releases](https://github.com/Tenpi/Music-Player/releases) tab. Updates can be installed automatically.
+
+### Bugs and Requests
+
+Open an issue on my GitHub repository.
+
+### Also See
+
+- [Music Player Web](https://github.com/Tenpi/Music-Player-Web)
---
+++ structures/functions.ts
@@ -0,0 +1,105 @@
+import fs from "fs"
+import path from "path"
+
+export default class Functions {
+    public static arrayIncludes = (str: string, arr: string[]) => {
+        for (let i = 0; i < arr.length; i++) {
+            if (str.includes(arr[i])) return true
+        }
+        return false
+    }
+
+    public static arrayRemove = <T>(arr: T[], val: T) => {
+        return arr.filter((item) => item !== val)
+    }
+
+    public static timeout = async (ms: number) => {
+        return new Promise((resolve) => setTimeout(resolve, ms))
+    }
+
+    public static removeDirectory = (dir: string) => {
+        if (!fs.existsSync(dir)) return
+        fs.readdirSync(dir).forEach((file: string) => {
+            const current = path.join(dir, file)
+            if (fs.lstatSync(current).isDirectory()) {
+                Functions.removeDirectory(current)
+            } else {
+                fs.unlinkSync(current)
+            }
+        })
+        try {
+            fs.rmdirSync(dir)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    public static logSlider = (position: number) => {
+        const minPos = 0
+        const maxPos = 1
+        const minValue = Math.log(60)
+        const maxValue = Math.log(100)
+        const scale = (maxValue - minValue) / (maxPos - minPos)
+        const value = Math.exp(minValue + scale * (position - minPos))
+        let adjusted = value - 100
+        if (adjusted > 0) adjusted = 0
+        return adjusted
+      }
+
+    public static formatSeconds = (duration: number) => {
+        let seconds = Math.floor(duration % 60) as any
+        let minutes = Math.floor((duration / 60) % 60) as any
+        let hours = Math.floor((duration / (60 * 60)) % 24) as any
+        if (Number.isNaN(seconds) || seconds < 0) seconds = 0
+        if (Number.isNaN(minutes) || minutes < 0) minutes = 0
+        if (Number.isNaN(hours) || hours < 0) hours = 0
+
+        hours = (hours === 0) ? "" : ((hours < 10) ? "0" + hours + ":" : hours + ":")
+        minutes = hours && (minutes < 10) ? "0" + minutes : minutes
+        seconds = (seconds < 10) ? "0" + seconds : seconds
+        return `${hours}${minutes}:${seconds}`
+    }
+
+    public static decodeEntities(encodedString: string) {
+        const regex = /&(nbsp|amp|quot|lt|gt);/g
+        const translate = {
+            nbsp:" ",
+            amp : "&",
+            quot: "\"",
+            lt  : "<",
+            gt  : ">"
+        } as any
+        return encodedString.replace(regex, function(match, entity) {
+            return translate[entity]
+        }).replace(/&#(\d+);/gi, function(match, numStr) {
+            const num = parseInt(numStr, 10)
+            return String.fromCharCode(num)
+        })
+    }
+
+    public static round = (value: number, step?: number) => {
+        if (!step) step = 1.0
+        const inverse = 1.0 / step
+        return Math.round(value * inverse) / inverse
+    }
+
+    public static streamToBuffer = async (stream: NodeJS.ReadableStream) => {
+        const chunks: any[] = []
+        const arr = await new Promise<Buffer>((resolve, reject) => {
+          stream.on("data", (chunk) => chunks.push(Buffer.from(chunk)))
+          stream.on("error", (err) => reject(err))
+          stream.on("end", () => resolve(Buffer.concat(chunks)))
+        })
+        return arr.buffer
+    }
+
+    public static getFile = async (filepath: string) => {
+        const blob = await fetch(filepath).then((r) => r.blob())
+        const name = path.basename(filepath).replace(".mp3", "").replace(".wav", "").replace(".flac", "").replace(".ogg", "")
+        // @ts-ignore
+        blob.lastModifiedDate = new Date()
+        // @ts-ignore
+        blob.name = name
+        return blob as File
+    }
+}
